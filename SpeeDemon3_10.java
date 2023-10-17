f1a00d9fdc25ba61b8d520e4bf017fa70a15ac22
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-17 16:34:02 +0200

    Recibiendo los datos de la API

--- SpeeDemon3_10.java
+++ SpeeDemon3_10.java
@@ -1,5 +1,6 @@
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * @author Antonio Ruiz Benito = speedemon
@@ -19,7 +20,35 @@ public class SpeeDemon3_10 {
             HttpURLConnection con = (HttpURLConnection)urlApi.openConnection();
 
             // Llamamos al request GET para hacer la solicitud
+            con.setRequestMethod("GET");
 
+            // Obtenemos el codigo de respuesta
+            // por ejemplo: 200, significa que el GET request ha sido satisfactorio
+            //  404 Not Found, el servidor no ha encontrado el resource solicitado
+            int responseCode = con.getResponseCode();
+
+            // Comprobamos si el codigo es distinto de 200
+            if(responseCode != 200) {
+
+                System.out.println("Error " + responseCode);
+
+            } else {
+                // Creamos un objeto StringBuilder donde se ira guardando la informacion mientras tenga espacio
+                StringBuilder infoString = new StringBuilder();
+
+                // El método openStream() de la url, inicia una nueva conexión con el servidor en el que se resuelve la URL.
+                // A continuación, se envía una solicitud HTTP GET a través de la conexión.
+                // Si todo va bien (es decir, 200 que ya hemos comprobado con anterioridad), el servidor devuelve
+                //  el mensaje de respuesta HTTP que lleva los datos.
+                Scanner sc = new Scanner(urlApi.openStream());
+
+                while(sc.hasNext()) {
+                    infoString.append(sc.nextLine());
+                }
+
+                sc.close();
+
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
