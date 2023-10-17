e836ed5d0e46f7e685c8474f0737ac0a5e0747ad
Antonio Ruiz <3speedemon3@gmail.com>
2023-10-17 16:24:38 +0200

    Conectando a la API

--- SpeeDemon3_10.java
+++ SpeeDemon3_10.java
@@ -1,12 +1,32 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * @author Antonio Ruiz Benito = speedemon
  */
 public class SpeeDemon3_10 {
-    // API
-    // https://developers.google.com/maps?hl=es-419
+
+    public static void main(String[] args) {
+
+        try {
+
+            String pokemon = "pikachu";
+
+            // Url con el enlace de la api
+            URL urlApi = new URL("https://pokeapi.co/api/v2/pokemon/"+ pokemon);
+
+            // Generaremos un objeto “HttpURLConnection para hacer la conexión a la api, mediante el método de la URL openConnection()
+            HttpURLConnection con = (HttpURLConnection)urlApi.openConnection();
+
+            // Llamamos al request GET para hacer la solicitud
 
 
-    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 
 }
